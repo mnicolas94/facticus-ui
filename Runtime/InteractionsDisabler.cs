55660c63aa96cf80326b5390959e5f258c6cbfb0
mnicolas94 <miguelcok27@gmail.com>
2024-01-11 10:25:08 -0500

    feat: implement InteractionsDisabler and create a prefab with it

---
+++ Runtime/InteractionsDisabler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class InteractionsDisabler : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+
+        private void OnEnable()
+        {
+            EnableInteractions();
+        }
+
+        public void EnableInteractions()
+        {
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
+        }
+
+        public void DisableInteractions()
+        {
+            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
+        }
+
+#if UNITY_EDITOR
+        
+        private void OnValidate()
+        {
+            if (_canvasGroup == null)
+            {
+                _canvasGroup = GetComponentInParent<CanvasGroup>(true);
+            }
+        }
+#endif
+    }
+}
